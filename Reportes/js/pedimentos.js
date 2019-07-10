385f32705e11136956eb25e3fa428a4f7d778530
Julio Hernández <armando10gl@gmail.com>
2019-07-10 16:46:59 -0500

    Se aplico una funcion para buscar el archivo

--- Reportes/js/pedimentos.js
+++ Reportes/js/pedimentos.js
@@ -15,15 +15,14 @@ $("#frmBusqueda").submit(function(e){
                 for(i = 0;i<val.length;i++) {
                     
                     
-                    html +="<tr><td>"+(i+1)+"</td><td>"+val[i][0]+"</td> <td>"+val[i][1]+"</td> <td>"+val[i][2]+"</td><td>"+val[i][3]+"</td> <td>"+val[i][4]+"</td> <td>"+val[i][5]+"</td><td>"+val[i][6]+"</td> ";
-                    
-                    if(fileExists("Anexos/PEDIMENTO/"+val[i][0])){ alert("olv");}
-                    
-                    html+="<td><a href='Anexos/PEDIMENTO/"+val[i][0]+".pdf'?><img title='Pedimento' src='img/pdf.png'></a>";
-                    html+="<a href='Anexos/FACTURA/"+val[i][0]+".pdf'?><img title='Factura' src='img/factura.png'></a>";
-                    html+="<a href='Anexos/CUENTAS MEXICANAS/"+val[i][0]+".pdf'?><img title='Cuentas Mexicanas' src='img/mex.png'></a>";
-                    html+="<a href='Anexos/CUENTAS AMERICANAS/"+val[i][0]+".pdf'?><img title='Cuentas Americanas' src='img/usa.png'></a>";
-                    html+="<a href='Anexos/HOJA DE CALCULO/"+val[i][0]+".pdf'?><img title='Cuentas Americanas' src='img/excel.png'></a>";
+                    html +="<tr><td>"+(i+1)+"</td><td>"+val[i][0]+"</td> <td>"+val[i][1]+"</td> <td>"+val[i][2]+"</td><td>"+val[i][3]+"</td> <td>"+val[i][4]+"</td> <td>"+val[i][5]+"</td><td>"+val[i][6]+"</td><td>";
+                    var ref = " ";
+                   
+                    if(fileExists("Anexos/PEDIMENTO/"+val[i][0]+".pdf")) html+="<a href='Anexos/PEDIMENTO/"+val[i][0]+".pdf'?><img title='Pedimento' src='img/pdf.png'></a>";
+                    if(fileExists("Anexos/FACTURA/"+val[i][0]+".pdf"))html+="<a href='Anexos/FACTURA/"+val[i][0]+".pdf'?><img title='Factura' src='img/factura.png'></a>";
+                    if(fileExists("Anexos/CUENTAS MEXICANAS/"+val[i][0]+".pdf"))html+="<a href='Anexos/CUENTAS MEXICANAS/"+val[i][0]+".pdf'?><img title='Cuentas Mexicanas' src='img/mex.png'></a>";
+                    if(fileExists("Anexos/CUENTAS AMERICANAS/"+val[i][0]+".pdf")) html+="<a href='Anexos/CUENTAS AMERICANAS/"+val[i][0]+".pdf'?><img title='Cuentas Americanas' src='img/usa.png'></a>";
+                    if(fileExists("Anexos/HOJA DE CALCULO/"+val[i][0]+".pdf"))html+="<a href='Anexos/HOJA DE CALCULO/"+val[i][0]+".pdf'?><img title='Cuentas Americanas' src='img/excel.png'></a>";
                     html+="</td></tr>";
                  }
                 html+="</tbody>";
@@ -33,10 +32,11 @@ $("#frmBusqueda").submit(function(e){
     });
 }
 function fileExists(path) {
-  try {
-    return fs.statSync(path).isFile();
-  } catch (e) {
-    return false;
-  }
+var response = $.ajax({
+		url: path,
+		type: 'HEAD',
+		async: false
+	}).status;   
+return (response != "200") ? false : true;
 }
                             

39d50cca88c12df7c310371a41c8a1db682b9b6b
Julio Hernández <armando10gl@gmail.com>
2019-07-12 14:41:58 -0500

    se agrego el documento en otra pestaña

--- Reportes/js/pedimentos.js
+++ Reportes/js/pedimentos.js
@@ -13,11 +13,11 @@ $("#frmBusqueda").submit(function(e){
                 for(i = 0;i<val.length;i++) {
                     html +="<tr><td>"+(i+1)+"</td><td>"+val[i][0]+"</td> <td>"+val[i][1]+"</td> <td>"+val[i][2]+"</td><td>"+val[i][3]+"</td> <td>"+val[i][4]+"</td> <td>"+val[i][5]+"</td><td>"+val[i][6]+"</td>";
                     var  doc =fileExists(val[i][0]+".pdf");   
-                    if(doc[0] == 1)html+="<td><a href='Anexos/PEDIMENTO/"+val[i][0]+".pdf'?><img title='Pedimento' src='img/pdf.png'></a></td>";else html+="<td></td>";
-                    if(doc[1]== 1) html+="<td><a href='Anexos/FACTURA/"+val[i][0]+".pdf'?><img title='Factura' src='img/factura.png'></a></td>";else html+="<td></td>";
-                    if(doc[2] == 1)html+="<td><a href='Anexos/CUENTAS MEXICANAS/"+val[i][0]+".pdf'?><img title='Cuentas Mexicanas' src='img/mex.png'></a></td>";else html+="<td></td>";
-                    if(doc[3] == 1) html+="<td><a href='Anexos/CUENTAS AMERICANAS/"+val[i][0]+".pdf'?><img title='Cuentas Americanas' src='img/usa.png'></a></td>";else html+="<td></td>";
-                    if(doc[4] == 1)html+="<td><a href='Anexos/HOJA DE CALCULO/"+val[i][0]+".pdf'?><img title='Cuentas Americanas' src='img/excel.png'></a></td>";else html+="<td></td>";
+                    if(doc[0] == 1)html+="<td><a href='Anexos/PEDIMENTO/"+val[i][0]+".pdf' target = '_blank' ><img title='Pedimento' src='img/pdf.png'></a></td>";else html+="<td></td>";
+                    if(doc[1]== 1) html+="<td><a href='Anexos/FACTURA/"+val[i][0]+".pdf' target = '_blank'><img title='Factura' src='img/factura.png'></a></td>";else html+="<td></td>";
+                    if(doc[2] == 1)html+="<td><a href='Anexos/CUENTAS MEXICANAS/"+val[i][0]+".pdf' target = '_blank'><img title='Cuentas Mexicanas' src='img/mex.png'></a></td>";else html+="<td></td>";
+                    if(doc[3] == 1) html+="<td><a href='Anexos/CUENTAS AMERICANAS/"+val[i][0]+".pdf' target = '_blank'><img title='Cuentas Americanas' src='img/usa.png'></a></td>";else html+="<td></td>";
+                    if(doc[4] == 1)html+="<td><a href='Anexos/HOJA DE CALCULO/"+val[i][0]+".pdf' target = '_blank'><img title='Cuentas Americanas' src='img/excel.png'></a></td>";else html+="<td></td>";
                     html+="</tr>";
                  }
                 html+="</tbody>";

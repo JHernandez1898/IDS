0af27d4e403e9f4cff7347c8405513e92ff5e976
Julio Hernández <armando10gl@gmail.com>
2019-07-12 14:32:17 -0500

    Se arreglaron cosas en el orden de la tabla

--- Reportes/js/pedimentos.js
+++ Reportes/js/pedimentos.js
@@ -9,22 +9,16 @@ $("#frmBusqueda").submit(function(e){
             {
                 $("#contenido").html(" ");
                 var val = eval(response);
-              
-                var html = " <tbody><tr><th>N°</th><th>Refererencia</th><th>Pedimento</th><th>Fecha</th><th>Cliente</th><th>Proveedor</th><th>Impo/Expo</th><th>Regimen </th><th>Anexo</th></tr>";
-      
+                var html = " <tbody><tr><th>N°</th><th>Refererencia</th><th>Pedimento</th><th>Fecha</th><th>Cliente</th><th>Proveedor</th><th>Impo/Expo</th><th>Regimen </th><th>PED</th><th>FAC</th><th>MXN</th><th>USA</th><th>CAL</th></tr>";
                 for(i = 0;i<val.length;i++) {
-                    
-                    
-                    html +="<tr><td>"+(i+1)+"</td><td>"+val[i][0]+"</td> <td>"+val[i][1]+"</td> <td>"+val[i][2]+"</td><td>"+val[i][3]+"</td> <td>"+val[i][4]+"</td> <td>"+val[i][5]+"</td><td>"+val[i][6]+"</td><td>";
-                    var ref = " ";
-                   
+                    html +="<tr><td>"+(i+1)+"</td><td>"+val[i][0]+"</td> <td>"+val[i][1]+"</td> <td>"+val[i][2]+"</td><td>"+val[i][3]+"</td> <td>"+val[i][4]+"</td> <td>"+val[i][5]+"</td><td>"+val[i][6]+"</td>";
                     var  doc =fileExists(val[i][0]+".pdf");   
-                    if(doc[0] == 1)html+="<a href='Anexos/PEDIMENTO/"+val[i][0]+".pdf'?><img title='Pedimento' src='img/pdf.png'></a>";
-                    if(doc[1]== 1)html+="<a href='Anexos/FACTURA/"+val[i][0]+".pdf'?><img title='Factura' src='img/factura.png'></a>";
-                    if(doc[2] == 1)html+="<a href='Anexos/CUENTAS MEXICANAS/"+val[i][0]+".pdf'?><img title='Cuentas Mexicanas' src='img/mex.png'></a>";
-                    if(doc[3] == 1) html+="<a href='Anexos/CUENTAS AMERICANAS/"+val[i][0]+".pdf'?><img title='Cuentas Americanas' src='img/usa.png'></a>";
-                    if(doc[4] == 1)html+="<a href='Anexos/HOJA DE CALCULO/"+val[i][0]+".pdf'?><img title='Cuentas Americanas' src='img/excel.png'></a>";
-                    html+="</td></tr>";
+                    if(doc[0] == 1)html+="<td><a href='Anexos/PEDIMENTO/"+val[i][0]+".pdf'?><img title='Pedimento' src='img/pdf.png'></a></td>";else html+="<td></td>";
+                    if(doc[1]== 1) html+="<td><a href='Anexos/FACTURA/"+val[i][0]+".pdf'?><img title='Factura' src='img/factura.png'></a></td>";else html+="<td></td>";
+                    if(doc[2] == 1)html+="<td><a href='Anexos/CUENTAS MEXICANAS/"+val[i][0]+".pdf'?><img title='Cuentas Mexicanas' src='img/mex.png'></a></td>";else html+="<td></td>";
+                    if(doc[3] == 1) html+="<td><a href='Anexos/CUENTAS AMERICANAS/"+val[i][0]+".pdf'?><img title='Cuentas Americanas' src='img/usa.png'></a></td>";else html+="<td></td>";
+                    if(doc[4] == 1)html+="<td><a href='Anexos/HOJA DE CALCULO/"+val[i][0]+".pdf'?><img title='Cuentas Americanas' src='img/excel.png'></a></td>";else html+="<td></td>";
+                    html+="</tr>";
                  }
                 html+="</tbody>";
                 $("#contenido").html(html);

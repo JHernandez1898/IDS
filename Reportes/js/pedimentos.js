f18523edfd2303f44291960a209c40ad31aa84ee
Julio Hernández <armando10gl@gmail.com>
2019-07-12 14:05:30 -0500

    Se cambio la forma de buscar los documentos

--- Reportes/js/pedimentos.js
+++ Reportes/js/pedimentos.js
@@ -18,11 +18,12 @@ $("#frmBusqueda").submit(function(e){
                     html +="<tr><td>"+(i+1)+"</td><td>"+val[i][0]+"</td> <td>"+val[i][1]+"</td> <td>"+val[i][2]+"</td><td>"+val[i][3]+"</td> <td>"+val[i][4]+"</td> <td>"+val[i][5]+"</td><td>"+val[i][6]+"</td><td>";
                     var ref = " ";
                    
-                    if(fileExists("Anexos/PEDIMENTO/"+val[i][0]+".pdf")) html+="<a href='Anexos/PEDIMENTO/"+val[i][0]+".pdf'?><img title='Pedimento' src='img/pdf.png'></a>";
-                    if(fileExists("Anexos/FACTURA/"+val[i][0]+".pdf"))html+="<a href='Anexos/FACTURA/"+val[i][0]+".pdf'?><img title='Factura' src='img/factura.png'></a>";
-                    if(fileExists("Anexos/CUENTAS MEXICANAS/"+val[i][0]+".pdf"))html+="<a href='Anexos/CUENTAS MEXICANAS/"+val[i][0]+".pdf'?><img title='Cuentas Mexicanas' src='img/mex.png'></a>";
-                    if(fileExists("Anexos/CUENTAS AMERICANAS/"+val[i][0]+".pdf")) html+="<a href='Anexos/CUENTAS AMERICANAS/"+val[i][0]+".pdf'?><img title='Cuentas Americanas' src='img/usa.png'></a>";
-                    if(fileExists("Anexos/HOJA DE CALCULO/"+val[i][0]+".pdf"))html+="<a href='Anexos/HOJA DE CALCULO/"+val[i][0]+".pdf'?><img title='Cuentas Americanas' src='img/excel.png'></a>";
+                    var  doc =fileExists(val[i][0]+".pdf");   
+                    if(doc[0] == 1)html+="<a href='Anexos/PEDIMENTO/"+val[i][0]+".pdf'?><img title='Pedimento' src='img/pdf.png'></a>";
+                    if(doc[1]== 1)html+="<a href='Anexos/FACTURA/"+val[i][0]+".pdf'?><img title='Factura' src='img/factura.png'></a>";
+                    if(doc[2] == 1)html+="<a href='Anexos/CUENTAS MEXICANAS/"+val[i][0]+".pdf'?><img title='Cuentas Mexicanas' src='img/mex.png'></a>";
+                    if(doc[3] == 1) html+="<a href='Anexos/CUENTAS AMERICANAS/"+val[i][0]+".pdf'?><img title='Cuentas Americanas' src='img/usa.png'></a>";
+                    if(doc[4] == 1)html+="<a href='Anexos/HOJA DE CALCULO/"+val[i][0]+".pdf'?><img title='Cuentas Americanas' src='img/excel.png'></a>";
                     html+="</td></tr>";
                  }
                 html+="</tbody>";
@@ -31,12 +32,18 @@ $("#frmBusqueda").submit(function(e){
         });
     });
 }
-function fileExists(path) {
-var response = $.ajax({
-		url: path,
-		type: 'HEAD',
-		async: false
-	}).status;   
-return (response != "200") ? false : true;
+function fileExists(name) {
+var doc ;
+$.ajax({
+        async: false,
+        type: "POST",
+		url: "php/BuscarDocumentos.php",
+        data: "name="+name,
+        success: function(response){
+            doc = eval(response);
+        }
+    });
+    return doc;
 }
+
                             
---
+++ Reportes/php/BuscarDocumentos.php
@@ -0,0 +1,17 @@
+<?php
+$Doc = array();
+$name = $_POST["name"];
+$rutapedimento  ="../Anexos/PEDIMENTO/";
+$rutafac = "../Anexos/FACTURA/";
+$rutamxn = "../Anexos/CUENTAS MEXICANAS/";
+$rutausa  = "../Anexos/CUENTAS AMERICANAS/";;
+$rutahcal ="../Anexos/HOJA DE CALCULO/";
+if(file_exists($rutapedimento.$name)) $Doc[]= 1;else $Doc[] = 0; 
+if(file_exists($rutafac.$name)) $Doc[] = 1;else $Doc[] = 0;
+if(file_exists($rutamxn.$name)) $Doc[] = 1;else $Doc[] = 0;
+if(file_exists($rutausa.$name)) $Doc[] = 1;else $Doc[] = 0;
+if(file_exists($rutahcal.$name)) $Doc[] = 1;else $Doc[] = 0;
+echo json_encode($Doc);
+?>
+
+    
